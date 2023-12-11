dd9349385ae6080827ce38bca8e09a7498d84e64
holy-boi <emeriebow78@gmail.com>
2023-12-11 05:05:17 +0100

    Rename index.html to index.php

---
+++ dist/index.php
@@ -0,0 +1,15 @@
+<?php
+$phpVersion = phpversion();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Version Checker</title>
+</head>
+<body>
+    <h1>PHP Version Checker</h1>
+    <p>The PHP version is: <?php echo $phpVersion; ?></p>
+</body>
+</html>
